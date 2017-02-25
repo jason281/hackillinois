109f1d99d73fdd87dc452a085cfa72d0dcaad4ca
jason281 <jason2888940@gmail.com>
2017-02-25 02:51:07 +0000

    build linux

---
+++ build_linux/CMakeFiles/run.dir/DependInfo.cmake
@@ -0,0 +1,38 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/admin/Desktop/hackillinois/gui/main.cpp" "/home/admin/Desktop/hackillinois/build_linux/CMakeFiles/run.dir/main.cpp.o"
+  "/home/admin/Desktop/hackillinois/gui/mainwindow.cpp" "/home/admin/Desktop/hackillinois/build_linux/CMakeFiles/run.dir/mainwindow.cpp.o"
+  "/home/admin/Desktop/hackillinois/build_linux/run_automoc.cpp" "/home/admin/Desktop/hackillinois/build_linux/CMakeFiles/run.dir/run_automoc.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_NO_DEBUG"
+  "QT_WIDGETS_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "/home/admin/Desktop/hackillinois/gui"
+  "/usr/include/qt"
+  "/usr/include/qt/QtWidgets"
+  "/usr/include/qt/QtGui"
+  "/usr/include/qt/QtCore"
+  "/usr/lib/qt/mkspecs/linux-g++"
+  "/usr/include/opencv"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build_linux/CMakeFiles/run_automoc.dir/AutomocOldMocDefinitions.cmake
@@ -0,0 +1,1 @@
+set(AM_OLD_COMPILE_SETTINGS "QT_CORE_LIB;QT_GUI_LIB;QT_NO_DEBUG;QT_WIDGETS_LIB ~~~ /home/admin/Desktop/hackillinois/build_linux;/home/admin/Desktop/hackillinois/gui;/usr/include/qt;/usr/include/qt/QtWidgets;/usr/include/qt/QtGui;/usr/include/qt/QtCore;/usr/lib/qt/mkspecs/linux-g++;/usr/include/opencv;/usr/include ~~~  ~~~ FALSE ~~~ ")
