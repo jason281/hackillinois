46f2a78cfc61f4b6f344a39ee1b0671bbcbeaad4
Pi-Wei Huang <abc1199281@gmail.com>
2017-02-25 08:57:05 +0000

    build rp3

---
+++ build_rp3/CMakeFiles/run.dir/DependInfo.cmake
@@ -0,0 +1,38 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/pi/Desktop/hackillinois/gui/main.cpp" "/home/pi/Desktop/hackillinois/build_rp3/CMakeFiles/run.dir/main.cpp.o"
+  "/home/pi/Desktop/hackillinois/gui/mainwindow.cpp" "/home/pi/Desktop/hackillinois/build_rp3/CMakeFiles/run.dir/mainwindow.cpp.o"
+  "/home/pi/Desktop/hackillinois/build_rp3/run_automoc.cpp" "/home/pi/Desktop/hackillinois/build_rp3/CMakeFiles/run.dir/run_automoc.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_NO_DEBUG"
+  "QT_WIDGETS_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "/home/pi/Desktop/hackillinois/gui"
+  "/usr/include/opencv"
+  "/usr/include/arm-linux-gnueabihf/qt5"
+  "/usr/include/arm-linux-gnueabihf/qt5/QtWidgets"
+  "/usr/include/arm-linux-gnueabihf/qt5/QtGui"
+  "/usr/include/arm-linux-gnueabihf/qt5/QtCore"
+  "/usr/lib/arm-linux-gnueabihf/qt5/mkspecs/linux-g++"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build_rp3/CMakeFiles/run.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "run_automoc.cpp"
+  "CMakeFiles/run.dir/main.cpp.o"
+  "CMakeFiles/run.dir/mainwindow.cpp.o"
+  "CMakeFiles/run.dir/run_automoc.cpp.o"
+  "run.pdb"
+  "run"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/run.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
